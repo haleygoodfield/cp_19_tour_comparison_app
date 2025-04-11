495c9d821c7d3e300fc1a3f981900802dcb66016
Haley Goodfield <haleygoodfield@usf.edu>
2025-04-10 21:03:36 -0400

    Build TourCard with conditional rendering and interaction

---
+++ src/components/TourCard.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+// Receives props for: id, name, info, price, image and callback onRemove 
+function TourCard({ id, name, info, image, price, onRemove }) {
+    const [readMore, setReadMore] = useState(false); // state to toggle 
+
+    return(
+        <article className="tour-card">
+            <img src={image} alt={name} /> {/* Display Tour image */}
+            <div className="card-info">
+                <h2>{name}</h2> {/* Display Tour name */}
+                <h3>${price}</h3> {/* Display Tour price */}
+                <p>{readMore ? info : `${info.substring(0, 200)}...`}</p> {/* Tour info with read more/ show less toggle */}
+                <button onClick={() => setReadMore(!readMore)}> {/* Toggle button */}
+                    {readMore ? 'Show Less' : 'Read More'} 
+                </button>
+                <button className="btn-remove" onClick={() => onRemove(id)}>Not Interested</button> {/* Button not "interested removes" tour */}
+            </div>
+        </article>
+    );
+}
+
+export default TourCard;
